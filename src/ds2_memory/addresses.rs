1e08e6b9a3b5734941fe12fb7830ce35dd33fab2
Dzavoy <dzavoy@proton.me>
2025-09-08 03:09:14 -0300

    added AOB patterns

--- src/ds2_memory/addresses.rs
+++ src/ds2_memory/addresses.rs
@@ -1,6 +1,23 @@
 use std::collections::HashMap;
 use lazy_static::lazy_static;
 
+pub const GAME_MANAGER_IMP: [Option<u8>; 17] = [
+    Some(0x48), Some(0x8B), Some(0x05), None, None, None, None, Some(0x48),
+    Some(0x8B), Some(0x58), Some(0x38), Some(0x48), Some(0x85), Some(0xDB),
+    Some(0x74), None, Some(0xF6),
+];
+
+pub const NET_SEASON_MANAGER: [Option<u8>; 17] = [
+    Some(0x48), Some(0x8B), Some(0x0D), None, None, None, None, Some(0x48),
+    Some(0x85), Some(0xC9), Some(0x74), None, Some(0x48), Some(0x8B),
+    Some(0x49), Some(0x18), Some(0xE8),
+];
+
+pub const KATANA_MAIN_APP: [Option<u8>; 12] = [
+    Some(0x48), Some(0x8B), Some(0x15), None, None, None, None, Some(0x45),
+    Some(0x32), Some(0xC0), Some(0x85), Some(0xC9),
+];
+
 lazy_static! {
     pub static ref ADDRESS_MAP: HashMap<&'static str, Vec<isize>> = {
         let mut map = HashMap::new();
