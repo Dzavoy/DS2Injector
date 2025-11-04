2c7ce9b5c407a19698cb2b4b6a72b3f9664d674b
Dzavoy <dzavoy@proton.me>
2025-11-04 17:39:24 -0300

    added new files to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
-pub mod mem_box;
+mod local_ptr;
+mod module_context;
+mod scan;
+
+pub use crate::local_ptr::{LocalPtr, LocalPtrChain};
+pub use crate::module_context::ModuleContext;
+pub use crate::scan::scan_bytes;
